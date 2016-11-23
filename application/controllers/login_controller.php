9dc42e57a89689a22c6a441a38ad7521ffd00c1c
Randhaja Malshan R. Wanigasekara <randhajapvt@gmail.com>
2016-11-23 17:46:08 +0530

    controller issue

--- application/controllers/login_controller.php
+++ application/controllers/login_controller.php
@@ -22,9 +22,12 @@ $this->load->model("login_model");
 
     	$result = $this->login_model->check_login($username, $password);
 
-    	if ($result) {
-    		$this->load->view('home');
-            //echo "eeeee";
+
+        foreach ($result->result_array() as $row) {
+
+
+    	if ($row['status']==0) {
+    		$this->load->view("manager_profile");
         	/*$data = array(
             	'is_logged' => true,
             	'username' => $this->input->post('username')
@@ -32,20 +35,15 @@ $this->load->model("login_model");
 
         	$this->session->set_userdata($data);*/
 
-    	} else {
-    		  ?>
-              <script type="text/javascript">
-                  alert("Wrong password or username");
-                   window.location='<?php echo base_url();?>'
-       
-              </script>
-<?php
-// $this->load->view('login_view');       
-     //       echo "vwrv";
-
+    	}elseif ($row['status']==1) {
+            $this->load->view('home');
+        
+        } else {
+    		echo "vwrv";
         	/*$this->form_validation->set_message('validate_user', $this->lang->line('error_login'));
         return false;*/
     }
  	}
+ }
  }
 ?>

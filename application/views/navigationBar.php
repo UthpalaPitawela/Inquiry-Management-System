70528b06b90ca8c2e96ad294e7aadc99807f6692
viduni94 <viduni.ushanka@gmail.com>
2016-11-24 03:32:43 +0530

    Add files via upload

---
+++ application/views/navigationBar.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+        <!-- META SECTION -->
+        <title>Edulink IMS</title>            
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <link rel="icon" href="EDULINK logo1.ico" type="image/x-icon" />
+        <!-- END META SECTION -->
+        
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
+        <!-- EOF CSS INCLUDE -->                                    
+    </head>
+
+    <body>
+        <!-- START PAGE CONTAINER -->
+        <div class="page-container">
+            
+            <!-- START PAGE SIDEBAR -->
+            <div class="page-sidebar">
+                <!-- START X-NAVIGATION -->
+                <ul class="x-navigation">
+                    <li class="xn-logo">
+                        <a href="index.html">Edulink IMS</a>
+                        
+                    </li>
+                    <li class="xn-profile">
+                        <div class="profile">
+                            <div class="profile-image">
+                                <img src="assets/images/users/defaultuser.jpg" alt="Viduni"/>
+                            </div>
+                            <div class="profile-data">
+                                <div class="profile-data-name">Name</div>
+                                <div class="profile-data-title">Designation</div>
+                            </div>
+                            <div class="profile-controls">
+                                <a href="#" class="profile-control-left"><span class="fa fa-info"></span></a>
+                                <a href="#" class="profile-control-right"><span class="fa fa-cog"></span></a>
+                            </div>
+                        </div>                                                                        
+                    </li>
+
+                    <li>
+                        <a href="#"><span class="fa fa-home"></span> <span class="xn-text">Home</span></a>                        
+                    </li>                    
+                    <li class="xn-openable">
+                        <a href="#"><span class="fa fa-question"></span> <span class="xn-text">Inquiries</span></a>
+                        <ul>
+                            <li><a href="#"><span class="fa fa-pencil"></span> Data Entry</a></li>
+                            <li><a href="#"><span class="fa fa-phone"></span> Manage Inquiries</a></li> 
+                            
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#"><span class="fa fa-database"></span> <span class="xn-text">Databases</span></a>
+                    </li>
+
+                    <li>
+                        <a href="#"><span class="fa fa-envelope"></span> <span class="xn-text">Email/SMS</span></a>
+                    </li>
+
+                    <li>
+                        <a href="#"><span class="fa fa-graduation-cap"></span> <span class="xn-text">Admissions</span></a>
+                    </li>
+
+                    <li>
+                        <a href="#"><span class="fa fa-user"></span> <span class="xn-text">Student Profiles</span></a>   
+                    </li>
+
+                    
+
+                    <li class="xn-title">Administration</li>
+                    <li>
+                        <a href="#"><span class="fa fa-bullseye"></span> <span class="xn-text">Targets</span></a>                        
+                    </li>    
+
+                    <li>
+                        <a href="#"><span class="fa fa-bar-chart-o"></span> <span class="xn-text">Reports</span></a>                 
+                    </li> 
+
+                    <li>
+                        <a href="#"><span class="fa fa-cogs"></span> <span class="xn-text">Settings</span></a>  
+                    </li>               
+
+                    
+                </ul>
+                <!-- END X-NAVIGATION -->
+            </div>
+            <!-- END PAGE SIDEBAR -->
+            
+            <!-- PAGE CONTENT -->
+            <div class="page-content">
+                
+                <!-- START X-NAVIGATION VERTICAL -->
+                <ul class="x-navigation x-navigation-horizontal x-navigation-panel">
+                    
+                    <!-- SEARCH -->
+                    <li class="xn-search">
+                        <form role="form">
+                            <input type="text" name="search" placeholder="Search..."/>
+                        </form>
+                    </li>   
+                    <!-- END SEARCH -->
+
+                    <!-- SIGN OUT -->
+                    <li class="xn-icon-button pull-right">
+                        <a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>                        
+                    </li> 
+                    <!-- END SIGN OUT -->
+
+                    <!-- SETTINGS -->
+                    <li class="xn-icon-button pull-right">
+                        <a href="#"><span class="fa fa-cog"></span></a>                        
+                    </li> 
+                    <!-- END SETTINGS -->
+
+                    
+                </ul>
+                <!-- END X-NAVIGATION VERTICAL -->                     
+
+                <!-- START BREADCRUMB -->
+                <ul class="breadcrumb">
+                    <li><a href="#">Home</a></li>                    
+                    <!-- <li class="active">Dashboard</li> -->
+                </ul>
+                <!-- END BREADCRUMB -->                       
+                
+                               
+            </div>            
+            <!-- END PAGE CONTENT -->
+        </div>
+        <!-- END PAGE CONTAINER -->
+
+        <!-- MESSAGE BOX-->
+        <div class="message-box animated fadeIn" id="mb-signout">
+            <div class="mb-container">
+                <div class="mb-middle">
+                    <div class="mb-title"><span class="fa fa-sign-out"></span> Log <strong>Out</strong> ?</div>
+                    <div class="mb-content">
+                        <p>Are you sure you want to log out?</p>                    
+                        <p>Press No if you want to continue working. Press Yes to logout current user.</p>
+                    </div>
+                    <div class="mb-footer">
+                        <div class="pull-right">
+                            <a href="pages-login.html" class="btn btn-success btn-lg">Yes</a>
+                            <button class="btn btn-default btn-lg mb-control-close">No</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END MESSAGE BOX-->
+         
+        
+    <!-- START SCRIPTS -->
+        <!-- START PLUGINS -->
+        <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>        
+        <!-- END PLUGINS -->
+
+        <!-- START THIS PAGE PLUGINS-->        
+        <script type='text/javascript' src='js/plugins/icheck/icheck.min.js'></script>        
+        <script type="text/javascript" src="js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
+        <script type="text/javascript" src="js/plugins/scrolltotop/scrolltopcontrol.js"></script>
+        
+        <script type="text/javascript" src="js/plugins/morris/raphael-min.js"></script>
+        <script type="text/javascript" src="js/plugins/morris/morris.min.js"></script>       
+        <script type="text/javascript" src="js/plugins/rickshaw/d3.v3.js"></script>
+        <script type="text/javascript" src="js/plugins/rickshaw/rickshaw.min.js"></script>
+        <script type='text/javascript' src='js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js'></script>
+        <script type='text/javascript' src='js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js'></script>                
+        <script type='text/javascript' src='js/plugins/bootstrap/bootstrap-datepicker.js'></script>                
+        <script type="text/javascript" src="js/plugins/owl/owl.carousel.min.js"></script>                 
+        
+        <script type="text/javascript" src="js/plugins/moment.min.js"></script>
+        <script type="text/javascript" src="js/plugins/daterangepicker/daterangepicker.js"></script>
+        <!-- END THIS PAGE PLUGINS-->        
+
+        <!-- START TEMPLATE -->
+        
+        <script type="text/javascript" src="js/plugins.js"></script>        
+        <script type="text/javascript" src="js/actions.js"></script>
+        
+        <script type="text/javascript" src="js/demo_dashboard.js"></script>
+        <!-- END TEMPLATE -->
+    <!-- END SCRIPTS -->         
+    </body>
+</html>
+
+
+
+
+
+

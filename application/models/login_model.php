49b7a79b9fe1749b00dd63cb320abbcb96a40155
Randhaja Malshan R. Wanigasekara <randhajapvt@gmail.com>
2016-11-23 17:51:53 +0530

    log in model issue

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -2,24 +2,14 @@
 class Login_Model extends CI_Model{
 	function check_login($username,$password){
 		
-		
-		$query = $this->db->query("SELECT * FROM user WHERE username= '$username' AND password = '$password'");
-        
 
-
-if ($query->num_rows() > 0) {
-foreach ($query ->result_array() as $rowuser) {
-
-$_SESSION["first_username"]=$rowuser['first_name'];
-}
-}
-
-
-        if ($query->num_rows() > 0) {
-            return true;
-        } else {
-            return false;
-        }
+		$query = $this->db->query("SELECT status FROM user WHERE username= '$username' AND password = '$password'");
+        return $query;
+        //if ($query->num_rows() > 0) {
+          //  return true;
+        //} else {
+          //  return false;
+        //}
 	}
 }
 ?>

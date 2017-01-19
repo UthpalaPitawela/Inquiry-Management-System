ef1fcd92ec233df590135ca12877552d3b5edf0e
RidmiShanika <ridmishanika1992@gmail.com>
2017-01-19 10:39:16 +0530

    Update view_register.php

--- application/views/view_register.php
+++ application/views/view_register.php
@@ -1,28 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
-    <head> 
-
+    <head>        
         <!-- META SECTION -->
         <title>Edulink IMS</title>            
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
-        
+         <link rel="icon" href="<?php echo base_url('public/assets/EDULINK-Logo1.ico');?>" type="image/x-icon" />
         <!-- END META SECTION -->
-         <link rel="icon" href="<?php echo base_url(); ?>EDULINK logo1.ico" type="image/x-icon" />               
-        <!-- CSS INCLUDE --> 
-             
-        <link rel="stylesheet" type="text/css" href= "<?php echo base_url('public/css/theme-default.css'); ?> "/>
-        <link rel="stylesheet" type="text/css" href= "<?php echo base_url('public/css/fontawesome/font-awesome.min.css'); ?> "/>
-        <!-- EOF CSS INCLUDE --> 
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/jquery/jquery.min.js"></script>
-        <!-- JS INCLUDE --> 
-
+        
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="<?php echo base_url('public/css/theme-default.css'); ?>"/>
+        <!-- EOF CSS INCLUDE -->                                    
     </head>
-    <body>
 
+    <body>
         <!-- START PAGE CONTAINER -->
         <div class="page-container">
             
@@ -53,12 +46,12 @@
                     <li>
                         <a href="#"><span class="fa fa-home"></span> <span class="xn-text">Home</span></a>                        
                     </li>                    
-                    <li class="xn-openable active">
+                    <li class="xn-openable">
                         
                         <a href="#"><span class="fa fa-question"></span> <span class="xn-text">Inquiries</span></a>
                         <ul>
                             <li class="active"><a href="<?php echo base_url();?>index.php/user"><span class="fa fa-pencil"></span> Data Entry</a></li>
-                            <li><a href="<?php echo base_url();?>index.php/manageInquiries_controller"><span class="fa fa-phone"></span> Manage Inquiries</a></li> 
+                            <li><a href="manageInquiries.php"><span class="fa fa-phone"></span> Manage Inquiries</a></li> 
                             
                         </ul>
                         
@@ -77,7 +70,7 @@
                     </li>
 
                     <li>
-                        <a href="<?php echo base_url();?>index.php/addStudentAcc_controller"><span class="fa fa-user"></span> <span class="xn-text">Student Profiles</span></a>   
+                        <a href="#"><span class="fa fa-user"></span> <span class="xn-text">Student Profiles</span></a>   
                     </li>
 
                     
@@ -99,38 +92,44 @@
                 </ul>
                 <!-- END X-NAVIGATION -->
             </div>
-            
-            
-<!-- ++++++++++++++++++++++++++++++++++++++   END OF SIDEBAR   +++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+
+<!-- ++++++++++++++++++++++++++++++++++++++++++ END of SIDEBAR +++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 
 <!-- PAGE CONTENT -->
-            <div class="page-content">
-                
-                <!-- START X-NAVIGATION VERTICAL -->
-                <ul class="x-navigation x-navigation-horizontal x-navigation-panel">
-                    
+<div class="page-content">
 
-                    <!-- SIGN OUT -->
-                    <li class="xn-icon-button pull-right">
-                        <a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>                        
-                    </li> 
-                    <!-- END SIGN OUT -->
+<!-- START X-NAVIGATION VERTICAL -->
+<ul class="x-navigation x-navigation-horizontal x-navigation-panel">
+    <!-- TOGGLE NAVIGATION -->
+    <li class="xn-icon-button">
+        <a href="#" class="x-navigation-minimize"><span class="fa fa-dedent"></span></a>
+    </li>
+    <!-- END TOGGLE NAVIGATION -->
+    <!-- SEARCH -->
+    <li class="xn-search">
+        <form role="form">
+            <input type="text" name="search" placeholder="Search..."/>
+        </form>
+    </li>
+    <!-- END SEARCH -->
+    <!-- SIGN OUT -->
+    <li class="xn-icon-button pull-right">
+        <a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>
+    </li>
+    <!-- END SIGN OUT -->
+    
+</ul>
+<!-- END X-NAVIGATION VERTICAL -->
 
-                   
+<!-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 
-                    
-                </ul>
-                <!-- END X-NAVIGATION VERTICAL -->                                      
-                
-                <!-- START BREADCRUMB -->
-                <ul class="breadcrumb">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Inquiries</a></li>
-                    <li><a href="<?php echo base_url();?>index.php/user">Data Entry</a></li>
-                </ul>
-                <!-- END BREADCRUMB -->
-                
-                <!-- PAGE CONTENT WRAPPER -->
+<!-- START BREADCRUMB -->
+<ul class="breadcrumb">
+
+</ul>
+<!-- END BREADCRUMB -->
+
+<!-- PAGE CONTENT WRAPPER -->
 <div class="page-content-wrap">
 
 <div class="row">
@@ -156,7 +155,7 @@
         <div class="col-md-6 col-xs-12">
             <div class="input-group">
                 <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                <input name="fname" type="text" class="form-control" required />
+                <input name="fname" type="text" class="form-control" required/>
             </div>
 
         </div>
@@ -207,7 +206,7 @@
         <div class="col-md-6 col-xs-12">
             <div class="input-group">
                 <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                <input type="text" name="email" class="form-control" required />
+                <input type="text" name="email" class="form-control" required/>
             </div>
 
         </div>
@@ -217,7 +216,7 @@
         <div class="col-md-6 col-xs-12">
             <div class="input-group">
                 <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                <input type="text" name="contactno" class="form-control" required />
+                <input type="text" name="contactno" class="form-control" required/>
             </div>
 
         </div>
@@ -234,41 +233,48 @@
 
         </div>
     </div>
-
     <div class="form-group">
         <label class="col-md-3 control-label">O/L Results</label>
         <div class="col-md-9 col-xs-12">
             <div class="row">
-                    <label class="col-md-10 col-xs-12 control-label" style="text-align: left">Above 5Cs, including Maths and English</label>
-            </div>
-             &nbsp;
 
+                <div class="col-md-10">
+                    <label class="col-md-10 col-xs-12 control-label">Above 5Cs including Maths and English</label>
+
+                </div>
+
+            </div>
             <div class="row">
+
+
                 <div class="col-md-3">
                     <input type="radio" name="ol" value="Yes"> Yes
                     <input type="radio" name="ol" value="No">No
+
+
+                </div>
+            </div>
+            <div class="row">
+                <label class="col-md-2 col-xs-12 control-label">Remark</label>
+                <div class="col-md-6 col-xs-12">
+                    <textarea class="form-control" rows="1" name="remark"></textarea>
+
                 </div>
             </div>
 
-        </div>
 
-        &nbsp;
-
-        <div class="form-group">
-        <label class="col-md-3 col-xs-12 control-label">Remarks</label>
-        <div class="col-md-6 col-xs-12">
-            <div class="input-group">
-                <textarea class="form-control" rows="3" cols="50" name="remark"></textarea>
-            </div>
-
-        </div>
         </div>
 
     </div>
+
+
 </div>
-
-
 <div class="col-md-6">
+
+
+
+
+
 
     <div class="form-group">
         <label class="col-md-3 control-label">A/L Results</label>
@@ -350,28 +356,28 @@
     <div class="form-group">
         <label class="col-md-3 col-xs-12 control-label">Mode Of Inquiry</label>
         <div class="col-md-6 col-xs-12" name="mode">
-            <input type="radio" name="mode" value="Walk-in"> &nbsp;Walk-in&nbsp;
-            <input type="radio" name="mode" value="Call"> &nbsp;Call&nbsp;            
-            <input type="radio" name="mode" value="Email"> &nbsp;Email / SMS&nbsp;
+            <input type="radio" name="mode" value="Walking"> Walking
+            <input type="radio" name="mode" value="Calls"> Calls
+            <input type="radio" name="mode" value="Email"> Email/SMS
 
         </div>
     </div>
     <div class="form-group">
-        <label class="col-md-3 col-xs-12 control-label">Potential Level </label>
+        <label class="col-md-3 col-xs-12 control-label">Potential Level For Next Intake </label>
         <div class="col-md-6 col-xs-12" name="mode">
-            <input type="radio" name="intake" value="High"> High&nbsp;
-            <input type="radio" name="intake" value="Medium"> Medium&nbsp;
-            <input type="radio" name="intake" value="Low"> Low&nbsp;
+            <input type="radio" name="intake" value="High"> High
+            <input type="radio" name="intake" value="Medium"> Medium
+            <input type="radio" name="intake" value="Low"> Low
 
         </div>
     </div>
 
     <div class="form-group">
-        <label class="col-md-3 col-xs-12 control-label">Potential Date </label>
+        <label class="col-md-3 col-xs-12 control-label">Potential Date Estimate</label>
         <div class="col-md-6 col-xs-12">
             <div class="input-group">
                 <span class="input-group-addon"><span class="fa fa-calendar"></span></span>
-                <input type="date" class="form-control datepicker" name="pdate" value="">
+                <input type="text" class="form-control datepicker" name="pdate" value="">
 
             </div>
 
@@ -381,36 +387,32 @@
     <div class="row">
         <label class="col-md-2 col-xs-12 control-label"></label>
         <div class="col-md-6 col-xs-12" id="ss">
-            <button class="btn btn-primary pull-right" id="eligle">See Eligible Degrees</button>
+            <button class="btn btn-primary pull-right" id="eligle">See Eligable Degrees</button>
 
         </div>
     </div>
-
-    &nbsp;
-
     <div class="form-group">
-        <label class="col-md-3 col-xs-12 control-label"></label>
+
+        <div class="col-md-9 col-xs-12"><b>
+
+            <textarea class="form-control" rows="3" name="ed" id="tt">
+
+            </textarea></b>
+
+
+
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-md-3 col-xs-12 control-label">Counsellor Name</label>
         <div class="col-md-6 col-xs-12">
             <div class="input-group">
-                <textarea class="form-control" rows="3" cols="50" name="ed" id="tt">
-
-            </textarea>
-
+                <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
+                <input name="counsellorname" type="text" class="form-control" />
             </div>
 
         </div>
     </div>
-
-      <div class="form-group">
-        <label class="col-md-3 col-xs-12 control-label">Counsellor Name</label>
-         <div class="col-md-6 col-xs-12">
-             <div class="input-group">
-                 <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                 <input name="counsellorname" type="text" class="form-control" />
-            </div>
- 
-         </div>
-     </div>
 
 
 </div>
@@ -430,41 +432,67 @@
 </div>
 
 </div>
-                <!-- END PAGE CONTENT WRAPPER -->                                                
-            </div>            
-            <!-- END PAGE CONTENT -->
+<!-- END PAGE CONTENT WRAPPER -->
+
+</div>
+<!-- END PAGE CONTENT -->
+</div>
+<!-- END PAGE CONTAINER -->
+
+<!-- MESSAGE BOX-->
+<div class="message-box animated fadeIn" data-sound="alert" id="mb-signout">
+    <div class="mb-container">
+        <div class="mb-middle">
+            <div class="mb-title"><span class="fa fa-sign-out"></span> Log <strong>Out</strong> ?</div>
+            <div class="mb-content">
+                <p>Are you sure you want to log out?</p>
+                <p>Press No if youwant to continue work. Press Yes to logout current user.</p>
+            </div>
+            <div class="mb-footer">
+                <div class="pull-right">
+                    <a href="pages-login.html" class="btn btn-success btn-lg">Yes</a>
+                    <button class="btn btn-default btn-lg mb-control-close">No</button>
+                </div>
+            </div>
         </div>
-        <!-- END PAGE CONTAINER -->
-        
-        
-                  
-        
-    <!-- START SCRIPTS -->
-        <!-- START PLUGINS -->
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/jquery/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/bootstrap/bootstrap.min.js"></script>                
-        <!-- END PLUGINS -->
-        
-        <!-- THIS PAGE PLUGINS -->
-        <script type='text/javascript' src='<?php echo base_url(); ?>public/js/plugins/icheck/icheck.min.js'></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
-        
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/bootstrap/bootstrap-datepicker.js"></script>                
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/bootstrap/bootstrap-file-input.js"></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/bootstrap/bootstrap-select.js"></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins/tagsinput/jquery.tagsinput.min.js"></script>
-        <!-- END THIS PAGE PLUGINS -->       
-        
-        <!-- START TEMPLATE -->
-        
-        
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/plugins.js"></script>        
-        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/actions.js"></script>        
-        <!-- END TEMPLATE -->
-    <!-- END SCRIPTS -->                   
-    </body>
+    </div>
+</div>
+<!-- END MESSAGE BOX-->
+
+<!-- START PRELOADS -->
+<audio id="audio-alert" src="audio/alert.mp3" preload="auto"></audio>
+<audio id="audio-fail" src="audio/fail.mp3" preload="auto"></audio>
+<!-- END PRELOADS -->
+
+<!-- START SCRIPTS -->
+<!-- START PLUGINS -->
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/jquery/jquery.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/jquery/jquery-ui.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/bootstrap/bootstrap.min.js'); ?>"></script>
+<!-- END PLUGINS -->
+
+<!-- THIS PAGE PLUGINS -->
+<script type='text/javascript' src='<?php echo base_url('assets/js/plugins/icheck/icheck.min.js'); ?>'></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js'); ?>"></script>
+
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/bootstrap/bootstrap-datepicker.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/bootstrap/bootstrap-file-input.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/bootstrap/bootstrap-select.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/tagsinput/jquery.tagsinput.min.js'); ?>"></script>
+<!-- END THIS PAGE PLUGINS -->
+
+<!-- START TEMPLATE -->
+<script type="text/javascript" src="<?php echo base_url('assets/js/settings.js'); ?>"></script>
+
+<script type="text/javascript" src="<?php echo base_url('assets/js/plugins.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/actions.js'); ?>"></script>
+
+
+<!-- END TEMPLATE -->
+<!-- END SCRIPTS -->
+</body>
 </html>
+
 
 
 <script>
@@ -517,6 +545,8 @@
                 var id = 1;
             }else if(tot>=240){
                 var id = 2;
+            }else if(tot>=180){
+                var id =3;
             }
             
             
@@ -540,10 +570,6 @@
 
 
 </script>
-
-
-
-
 
 
 

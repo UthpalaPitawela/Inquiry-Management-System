e3eb5cf093b4da589fe1a1749bdb2f9a8a256a8c
Randhaja Malshan R. Wanigasekara <randhajapvt@gmail.com>
2017-01-23 01:55:21 +0530

    gg
    gg

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -9,30 +9,60 @@
                             $subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,$counsellorname){
          //$shal_password = sha1($password);
 
+?>
+<script type="text/javascript">
+    alert("abc");
+</script>
+<?php
+
+
+
+
          $total = $grade1 + $grade2 + $grade3;
          if(($ol=="Yes") &&($total >= 260)){
              $query_str = "INSERT INTO register(Fname,Lname,Address,Country,Email,Gender,Contactno,NIC,OL,Remark,Subject1,
-          Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+          Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES ($fname,$Lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,$subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,1,$counsellorname)";
 
-             $this->db->query($query_str,array($fname,$lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,
-                 $subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,1,$counsellorname));
+             $this->db->query($query_str);
+
+             // ,array($fname,$lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,
+             //     $subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,1,$counsellorname)
 
             // $a = $this->db->query("SELECT Programme1,Programme2 FROM programme WHERE id = 1");
              //return $a->result();
 
          }else if(($ol == "Yes") && ((260 > $total) && ($total >= 240))){
-             $query_str = "INSERT INTO register(Fname,Lname,Address,Country,Email,Gender,Contactno,NIC,OL,Remark,Subject1,
-          Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-             $this->db->query($query_str,array($fname,$lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,
-                 $subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,2,$counsellorname));
+             $query_str = "INSERT INTO register(Fname,Lname,Address,Country,Email,Gender,Contactno,NIC,OL,Remark,Subject1,
+          Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES ($fname,$Lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,$subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,2,$counsellorname)";
+
+
+          //    $query_str = "INSERT INTO register(Fname,Lname,Address,Country,Email,Gender,Contactno,NIC,OL,Remark,Subject1,
+          // Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+             $this->db->query($query_str);
+
+             // ,array($fname,$lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,
+             //     $subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,2,$counsellorname)
 
          }else if(($ol == "Yes") && (180 >= $total)){
-            $query_str = "INSERT INTO register(Fname,Lname,Address,Country,Email,Gender,Contactno,NIC,OL,Remark,Subject1,
-          Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-            $this->db->query($query_str,array($fname,$lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,
-            $subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,3,$counsellorname));
+
+             $query_str = "INSERT INTO register(Fname,Lname,Address,Country,Email,Gender,Contactno,NIC,OL,Remark,Subject1,
+          Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES ($fname,$Lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,$subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,3,$counsellorname)";
+
+
+          //   $query_str = "INSERT INTO register(Fname,Lname,Address,Country,Email,Gender,Contactno,NIC,OL,Remark,Subject1,
+          // Subject2,Subject3,Grade1,Grade2,Grade3,Other,Inquiry,Intake,Pdate,id,CounsellorName)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+            $this->db->query($query_str);
+
+
+
+            // ,array($fname,$lname,$address,$country,$email,$gender,$contactno,$nic,$ol,$remark,$subject1,$subject2,
+            // $subject3,$grade1,$grade2,$grade3,$other,$mode,$intake,$pdate,3,$counsellorname)
+
+
 
             }
          function select_degree(){

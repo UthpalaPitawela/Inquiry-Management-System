bb71383df13f9768370d6ae0dcaa84c69b3dcdcf
malshanucsc <randhajapvt@gmail.com>
2017-02-19 22:15:51 +0530

    reminder okay.

--- application/controllers/Counsellor_Profile_Controller.php
+++ application/controllers/Counsellor_Profile_Controller.php
@@ -3,15 +3,16 @@ class Counsellor_Profile_Controller extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->model('Counsellor_Profile_Model');
-
+		$this->load->model("Student_Data_Model");
 	}
 	function index($name,$lname){
 		$counsellorname = $name." ".$lname; 
+		 $data['remiderstudent'] = $this->Student_Data_Model->get_Student_For_Counsellor($counsellorname);
 		$val = $this->Counsellor_Profile_Model->getActivitySummary($counsellorname);
 		$data['result'] = $val[0];
-		$this->load->view('counsellor_profile',$data);
+		$this->load->view('Counsellor_Profile',$data);
 	}
 }
 
 
-		
+		 

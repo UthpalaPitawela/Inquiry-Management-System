0eca09bf4d46aec31b67cabb39f6a61ec35456fd
uthpalapitawela <uthpala.isiru@gmail.com>
2017-04-03 10:12:52 +0530

    ewb inquiries model changed

--- application/models/Web_Inquiries_Model.php
+++ application/models/Web_Inquiries_Model.php
@@ -11,7 +11,7 @@ class Web_Inquiries_Model extends CI_Model{
 	}
 
 	function getInquiries(){
-		$query = $this->db->query("SELECT Fname,Lname,Email,Contactno,Intake,r_id FROM register WHERE CounsellorName IS NULL AND Status = 'Pending' AND account_created  = 1 ");
+		$query = $this->db->query("SELECT Fname,Lname,Email,Contactno,Intake,r_id FROM register WHERE CounsellorName IS NULL AND Status = 'Pending' AND web  = 1 ");
 		return $query;
 	}
 

6a3584269b45a1002f0920f73d8e28c04adedeb5
HariniSamarasinghe <harini.yasodhya@gmail.com>
2017-04-04 13:56:15 +0530

    changes the SMS api details and edited sms views

--- application/views/manageInquiries_view.php
+++ application/views/manageInquiries_view.php
@@ -19,18 +19,6 @@
         <!-- EOF CSS INCLUDE --> 
 
         <style type="text/css">
-
-        #namediv{
-            color:white;
-
-        }
-        #tagresultdiv{
-            color:white;
-        }
-        #contactnodiv{
-            color: white;
-
-        }
             input:focus{
                 outline: none;
             }
@@ -166,27 +154,13 @@
                 <!-- START X-NAVIGATION VERTICAL -->
                 <ul class="x-navigation x-navigation-horizontal x-navigation-panel">
 
-                <li class="xn-search" style="z-index: 1001;">
-                        <form role="form">
-                            <input type="text" style="width: 200px" id="searchname" name="search" onkeyup="nameSearch() " placeholder="Search by Name">
-                            <div id="namediv" style="background-color: black; " ></div> 
-                        </form>
-                </li>
-
-                <li class="xn-search" style="z-index: 1001;">
-                        <form role="form">
-                            <input type="text" style="width: 200px" id="searchcontactno" name="search" onkeyup="ContactNOSearch()" placeholder="Search by Contact No.">
-                            <div id="contactnodiv" style="background-color: black; " ></div> 
-                        </form>
-                </li>
-                
 
                 <li class="xn-search" style="z-index: 1001;">
                         <form role="form">
                             <input type="text" style="width: 200px" id="searchtag" name="search" onkeyup="tagsinput()" placeholder="Search Tags">
                             <div id="tagresultdiv" style="background-color: black; " ></div> 
                         </form>
-                </li>
+                    </li>
                     
 
                     <!-- SIGN OUT -->
@@ -699,7 +673,7 @@ if(   ( strtotime($today) >= strtotime($potentialdate) ) || ( (strtotime($thisse
                                                                         <label class="col-md-3 col-xs-12 control-label" for="summary1">Enter Summary:</label>
                                                                         <div class="col-md-8 col-xs-12">     
                                                                             
-                                                                            <textarea class="form-control" rows="5" name="other" id="other"></textarea>
+                                                                            <textarea class="form-control" rows="5" name="summary1" id="summary1"></textarea>
                                                                         </div>
                                                                     </div>
                                                                 
@@ -1067,7 +1041,7 @@ if(   ( ( strtotime($today) >= strtotime($thisjanend) ) && ( strtotime($today) <
                                                                         <label class="col-md-3 col-xs-12 control-label" for="summary1">Enter Summary:</label>
                                                                         <div class="col-md-8 col-xs-12">     
                                                                             
-                                                                            <textarea class="form-control" rows="5" name="other" id="other"></textarea>
+                                                                            <textarea class="form-control" rows="5" name="summary1" id="summary1"></textarea>
                                                                         </div>
                                                                     </div>
                                                                 
@@ -1419,7 +1393,7 @@ if(   ( ( strtotime($today) > strtotime($thisjanend) ) && ( strtotime($today) < 
                                                                         <label class="col-md-3 col-xs-12 control-label" for="summary1">Enter Summary:</label>
                                                                         <div class="col-md-8 col-xs-12">     
                                                                             
-                                                                            <textarea class="form-control" rows="5" name="other" id="other"></textarea>
+                                                                            <textarea class="form-control" rows="5" name="summary1" id="summary1"></textarea>
                                                                         </div>
                                                                     </div>
                                                                 
@@ -1789,34 +1763,7 @@ $.ajax({
 
 $(document).click(function() {
     $("#tagresultdiv").html("");
-    $("#namediv").html("");
-    $("#contactnodiv").html("");
-
 });
-
-$(document).ready(function () {
-   $('#searchname').click(function() {
-    $("#tagresultdiv").html("");
-   // $("#namediv").html("");
-    $("#contactnodiv").html("");
-
-});
-
-   $('#searchtag').click(function() {
-    //$("#tagresultdiv").html("");
-   $("#namediv").html("");
-    $("#contactnodiv").html("");
-
-});
-
-   $('#searchcontactno').click(function() {
-    $("#tagresultdiv").html("");
-    $("#namediv").html("");
-   // $("#contactnodiv").html("");
-
-});
-});
-
 
 
     function tagsinput(){
@@ -1824,7 +1771,7 @@ $(document).ready(function () {
  var searchtag = document.getElementById("searchtag").value;
 
 
-    $('#tagresultdiv').html("No Result");
+    $('#tagresultdiv').html("");
      // alert(skey);
 
 
@@ -1867,124 +1814,6 @@ $.ajax({
 
     }
 </script>
-
-<script type="text/javascript">
-
-
-
-    function nameSearch(){
-
- var searchkey = document.getElementById("searchname").value;
-
-
-    $('#namediv').html("No Result");
-     // alert(skey);
-
-
-    if(searchkey!=""){
-
-$.ajax({             
-                     type:"post",
-                     url : '<?php echo base_url();?>index.php/ManageInquiries_controller/searchbyName/',
-                     data : {skey:searchkey},
-                     success: function(searchresult) {
-
-                            $('#namediv').html(searchresult);
-                            
-
-                     },
-                     error: function(jqXHR){
-                       alert(jqXHR.responseText);
-                        //jqXHR.responseText
-                     }
-                 });
-               
-
-
-
-            // $.ajax({
-
-            //     type:"get",
-            //     url : '<?php echo base_url();?>index.php/Tag_Controller/searchTag/'+searchtag,
-            //     success: function (searchresult) {
-
-
-            //         $('#tagdiv').html(searchresult);
- 
-            //     }
-            // })
-
-
-    }
-
-
-    }
-</script>
-
-
-
-
-
-
-
-
-<script type="text/javascript">
-
-
-
-    function ContactNOSearch(){
-
- var searchkey = document.getElementById("searchcontactno").value;
-
-
-    $('#contactnodiv').html("No Result");
-     // alert(skey);
-
-
-    if(searchkey!=""){
-
-$.ajax({             
-                     type:"post",
-                     url : '<?php echo base_url();?>index.php/ManageInquiries_controller/searchbyNo/',
-                     data : {skey:searchkey},
-                     success: function(searchresult) {
-
-                            $('#contactnodiv').html(searchresult);
-                            
-
-                     },
-                     error: function(jqXHR){
-                       alert(jqXHR.responseText);
-                        //jqXHR.responseText
-                     }
-                 });
-               
-
-
-
-            // $.ajax({
-
-            //     type:"get",
-            //     url : '<?php echo base_url();?>index.php/Tag_Controller/searchTag/'+searchtag,
-            //     success: function (searchresult) {
-
-
-            //         $('#tagdiv').html(searchresult);
- 
-            //     }
-            // })
-
-
-    }
-
-
-    }
-</script>
-
-
-
-
-
 
 <!-- Start of alerts -->
 <?php
